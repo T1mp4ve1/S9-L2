d9accc97690eff227fe1ac72af71a67ba50c4bb7
T1mp4ve1 <pavelwire@outlook.it>
2025-09-30 15:28:09 +0200

    Alert

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MyCustomNav from "./components/MyCustomNav";
 import MyCustomFooter from "./components/MyCustomFooter";
+import MyCustomAlert from "./components/MyCustomAlert";
 
 function App() {
   return (
     <>
       <MyCustomNav />
+      <MyCustomAlert />
       <MyCustomFooter />
     </>
   );
