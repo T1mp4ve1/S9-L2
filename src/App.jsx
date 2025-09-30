9bbecb4788d88b5c7632b8514e4bcbe4d45c975e
T1mp4ve1 <pavelwire@outlook.it>
2025-09-30 14:52:03 +0200

    bootstrap install

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyCustomNav from "./components/MyCustomNav";
+
+function App() {
+  return (
+    <>
+      <MyCustomNav />
+    </>
+  );
+}
+
+export default App;
