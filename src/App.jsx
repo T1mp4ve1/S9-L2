aeff64aaca3b00d4131588bf4e92361b50ee4b5b
T1mp4ve1 <pavelwire@outlook.it>
2025-09-30 15:20:11 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MyCustomNav from "./components/MyCustomNav";
+import MyCustomFooter from "./components/MyCustomFooter";
 
 function App() {
   return (
     <>
       <MyCustomNav />
+      <MyCustomFooter />
     </>
   );
 }
